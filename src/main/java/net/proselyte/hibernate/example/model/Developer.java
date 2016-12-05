6c27e0c4cf8f0fa26837a6079ad48ecc1a192bf9
Raketa <detroi.vv@gmail.com>
2016-12-05 02:40:07 +0200

    All project files

---
+++ src/main/java/net/proselyte/hibernate/example/model/Developer.java
@@ -0,0 +1,82 @@
+package net.proselyte.hibernate.example.model;
+
+public class Developer {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String specialty;
+    private int experience;
+
+    /**
+     * Default Constructor
+     */
+    public Developer() {
+    }
+
+    /**
+     * Plain constructor
+     */
+    public Developer(String firstName, String lastName, String specialty, int experience) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.specialty = specialty;
+        this.experience = experience;
+    }
+
+
+    /**
+     * Getters and Setters
+     */
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSpecialty() {
+        return specialty;
+    }
+
+    public void setSpecialty(String specialty) {
+        this.specialty = specialty;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public void setExperience(int experience) {
+        this.experience = experience;
+    }
+
+    /**
+     * toString method (optional)
+     */
+    @Override
+    public String toString() {
+        return "Developer:\n" +
+                "id: " + id +
+                "\nFirst Name: " + firstName + "\n" +
+                "Last Name: " + lastName + "\n" +
+                "Specialty: " + specialty + "\n" +
+                "Experience: " + experience + "\n";
+    }
+}
